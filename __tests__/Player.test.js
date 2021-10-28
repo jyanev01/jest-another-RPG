90340ad638c511597d08291c57b9474df6c7f626
jyanev01 <jordanyanev@gmail.com>
2021-10-27 23:33:42 -0400

    working fixed spelling errors

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,7 @@
-const Potion = require('../lib/Potion');
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
-
-console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -11,9 +10,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(
-        expect.arrayContaining([expect.any(Object)])
-    );
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
 test("gets player's stats as an object", () => {
